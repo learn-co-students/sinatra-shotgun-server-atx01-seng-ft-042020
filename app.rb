8ddf09d651cc23dc02d08ee1ebd9415ef19ba88b
marshall405 <marshall.slemp@gmail.com>
2020-05-11 21:28:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,39 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I  THIS! "
+  end
+
+  get '/hello' do
+  "
+  <style>
+    h1   {color: #000;}
+    nav {
+      background: lightgray;
+    }
+    ul {
+      color: #fff;
+      display: flex;
+      list-style: none;
+      border: 1px solid black;
+    }
+    a {
+      color: inherit;
+    }
+    li {
+      margin: 5px;
+    }
+  </style>
+  
+  <h1>Hello, #{params['name']}</h1>
+  <nav>
+    <ul>
+      <a href=http://www.marshallslemp.com><li> About </li></a>
+      <a href=#top><li> Contact </li></a>
+      <a href=#top><li> Blog </li></a>
+    </ul>
+  </nav>
+  "
   end
 
 end
